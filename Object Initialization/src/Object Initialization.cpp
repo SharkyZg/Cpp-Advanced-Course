b1688d1ad007eb4386b2c87237db7a63979677fb
Marko Sarkanj <marko.sarkanj@gmail.com>
2017-11-29 07:34:00 +0100

    lecture 48

---
+++ Object Initialization/src/Object Initialization.cpp
@@ -0,0 +1,43 @@
+//============================================================================
+// Name        : Object.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class Test {
+	int id{3};
+	string name{"Mike"};
+
+public:
+	Test() = default;
+	Test(const Test &other) = delete;
+	Test &operator=(const Test &other) = delete;
+
+	Test(int id) : id(id){
+		}
+	void print() {
+		cout << id << ": " << name << endl;
+	}
+
+
+};
+
+int main() {
+	Test test;
+
+	test.print();
+
+	Test test1(77);
+	test1.print();
+
+//	Won't work we deleted them
+//	Test test2 = test1;
+//	test2 = test;
+
+	return 0;
+}
